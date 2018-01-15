654c4f0be6bf6ee966fdff8bfcf0955f25ac301b
abd <6nazmi@gmailc.om>
2018-01-15 14:52:34 +0200

    Grasshopper - Summation

--- src/script8.js
+++ src/script8.js
@@ -55,3 +55,27 @@ function findNeedle(haystack) {
     }
   }
 
+/**
+ * Grasshopper - Summation
+ 
+Summation
+
+Write a program that finds the summation of every number between 1 and num. 
+The number will always be a positive integer greater than 0.
+
+For example:
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+ */
+
+var summation = function (num) {
+    var sum=1;
+    for(var i=num;i>1;i--){
+        sum=sum+i;
+    }
+    return sum;
+  }
