7cf2d5fd11270ebb3a3624ae1f83cc390cb0455a
abd <6nazmi@gmailc.om>
2018-01-28 16:03:50 +0200

    Find the missing term in an Arithmetic Progression

--- src/script8.js
+++ src/script8.js
@@ -146,3 +146,47 @@ function deepCount(a){
     }
     return numofAllElment;
   }
+
+
+/**
+ * Find the missing term in an Arithmetic Progression
+An Arithmetic Progression is defined as one in which there is a constant difference between
+ the consecutive terms of a given series of numbers. 
+ You are provided with consecutive elements of an Arithmetic Progression. 
+ There is however one hitch: exactly one term from the original series is missing from the set of numbers
+  which have been given to you. The rest of the given series is the same as the original AP.
+  Find the missing term.
+
+You have to write the function findMissing(list), 
+list will always be at least 3 numbers. The missing term will never be the first or last one.
+
+Example :
+
+findMissing([1,3,5,9,11]) == 7
+PS: This is a sample question of the facebook engineer challenge on interviewstreet. I found it quite fun to solve on paper using math, derive the algo that way. Have fun!
+
+sample 
+[1,3,5,9,11]
+[1, 3, 4]
+[5,15,20]
+[9,27,36,45]
+[10,8,4,2]
+[1,2,3,4]
+
+Role
+at least 3 elment 
+the missing never be a first or last element  
+hints
+Common Difference = lastelment - first elment / lenth-1
+ */
+var findMissing = function (list) {  
+    var missingElement;
+    var commonDifference=(list[list.length - 1] - list[0]) / list.length;
+    for(var i = 0 ;i < list.length -1;i++){
+        if((list[i+1]-list[i])!==commonDifference){
+            missingElement=list[i]+commonDifference;
+        }
+    }
+    return missingElement ;
+    
+  }
