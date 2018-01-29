f2b50081785d378ea6e8db428cbb018478540e22
abd <6nazmi@gmailc.om>
2018-01-29 16:03:59 +0200

    Calculating with Functions

--- src/script8.js
+++ src/script8.js
@@ -219,3 +219,77 @@ function digital_root(n) {
    }
    
   }
+
+/**
+ * Calculating with Functions
+This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+
+Requirements:
+
+There must be a function for each number from 0 ("zero") to 9 ("nine")
+There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy (divided_by in Ruby)
+Each calculation consist of exactly one operation and two numbers
+The most outer function represents the left operand, the most inner function represents the right operand
+ 
+seven(times(five())); // must return 35
+four(plus(nine())); // must return 13
+eight(minus(three())); // must return 5
+six(dividedBy(two())); // must return 3
+*/
+
+function recur(n, op) { return (op) ? op.call(op, n) : n; }
+function zero(op)     { return recur(0, op); }
+function one(op)      { return recur(1, op); }
+function two(op)      { return recur(2, op); }
+function three(op)    { return recur(3, op); }
+function four(op)     { return recur(4, op); }
+function five(op)     { return recur(5, op); }
+function six(op)      { return recur(6, op); }
+function seven(op)    { return recur(7, op); }
+function eight(op)    { return recur(8, op); }
+function nine(op)     { return recur(9, op); }
+
+function plus(num) {
+    return function(res) {
+        return res + num;
+    };
+}
+function minus(num) {
+    return function(res) {
+        return res - num;
+    };
+}
+function times(num) {
+    return function(res) {
+        return res * num;
+    };
+}
+function dividedBy(num) {
+    return function(res) {
+        return res / num;
+    };
+}
+
+//clever solotion 
+
+var n = function(digit) {
+    return function(op) {
+      return op ? op(digit) : digit;
+    }
+  };
+  var zero = n(0);
+  var one = n(1);
+  var two = n(2);
+  var three = n(3);
+  var four = n(4);
+  var five = n(5);
+  var six = n(6);
+  var seven = n(7);
+  var eight = n(8);
+  var nine = n(9);
+  
+  function plus(r) { return function(l) { return l + r; }; }
+  function minus(r) { return function(l) { return l - r; }; }
+  function times(r) { return function(l) { return l * r; }; }
+  function dividedBy(r) { return function(l) { return l / r; }; }
